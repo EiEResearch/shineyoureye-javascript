851f26af92193d8323ced0f79281b8433c6a1701
EiEResearch <pmsye@eienigeria.org>
2020-04-06 09:29:37 +0200

    Included Facebook Comment

--- src/app/shared/components/PageComments.vue
+++ src/app/shared/components/PageComments.vue
@@ -4,17 +4,26 @@
       <div class="row">
         <div class="col-lg-8 col-md-10 mx-auto mt-5">
           <div class="embed-responsive embed-responsive-item">
-            <div id="fb-root" />
-            <div class="fb-comments" />
+            <div class="fb-comments"
+                 :data-href="url"
+                 data-width=""
+                 data-numposts="5"
+            />
           </div>
         </div>
       </div>
+      <div id="fb-root" />
     </div>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      url: process.env.VUe_APP_URL,
+    };
+  },
   mounted() {
     // eslint-disable-next-line func-names
     (function (d, s, id) {
@@ -23,8 +32,10 @@ export default {
       if (d.getElementById(id)) return;
       // eslint-disable-next-line prefer-const
       js = d.createElement(s); js.id = id;
-      js.src = '//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.8';
+      js.src = '//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v6.0&appId=647627356057438&autoLogAppEvents=1';
       js.async = true;
+      js.defer = true;
+      js.crossorigin = 'anonymous';
       fjs.parentNode.insertBefore(js, fjs);
     }(document, 'script', 'facebook-jssdk'));
   },
