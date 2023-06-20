02b60b1cc4345021d4132eca5bd2296ab0770614
EiEResearch <pmsye@eienigeria.org>
2023-06-20 14:40:15 +0100

    Update Home.vue
    Adjusting the max-length on line 29

--- src/app/home/Home.vue
+++ src/app/home/Home.vue
@@ -26,7 +26,7 @@
               <div class='row my-3'>
                 <h4 class='align-items-center'>
                   We are currently in the process of updating our website with the profiles and contact details of the newly elected and appointed government officials. <br />
-                  If you have any urgent inquiries or require immediate assistance, please do not hesitate to contact our customer support team at <br /><strong>syeinfo@eienigeria.org</strong>. We are here to help.
+                  If you have any urgent inquiries or require immediate assistance, please do not hesitate to <br />contact our customer support team at <strong>syeinfo@eienigeria.org</strong>. We are here to help.
                 </h4>
               </div>
             </div>
